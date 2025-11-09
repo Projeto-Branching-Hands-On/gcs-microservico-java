584fe1b45e199bc6efc2c488f0a0ffa879361f25
Gabriel Vitorio Goncalves <104917512+GabrielVitorioGoncalves@users.noreply.github.com>
2025-11-09 19:28:59 -0300

    test(demo): adicionar teste intencionalmente falhando (DummyTest) para validar pipeline

---
+++ src/test/java/com/example/demo/DummyTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class DummyTest {
+
+    @Test
+    void deveFalhar() {
+        fail("Erro proposital para testar o pipeline");
+    }
+}
